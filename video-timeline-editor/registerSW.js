d4d3bbed932ddc9f5939f4037abeb59790c37798
deftec96-crypto <deftec96@gmail.com>
2026-06-07 08:37:50 +0900

    Deploy PWA 2026-06-06T23:37:49.797Z

---
+++ video-timeline-editor/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/app-release/video-timeline-editor/sw.js', { scope: '/app-release/video-timeline-editor/' })})}
